bfeb3615fd0a7f773e1e87f2cf9f0df960a9311f
Demunter Bao <48960850+Zareon-Feryel@users.noreply.github.com>
2024-11-22 03:18:19 +0100

    Update change-directory.ps1

--- powershell/profile/change-directory.ps1
+++ powershell/profile/change-directory.ps1
@@ -1,4 +1,5 @@
 function Change-Directory {
+    # Change 'D:\Programmation\' with the full path to your projects folder
     Set-Location D:\Programation\
 }
 Set-Alias -Name repos -Value Change-Directory
